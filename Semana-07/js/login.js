763309c84325bc8c9b3bd7594b9149e59908ebf3
GuidoCerioni <guidocerioni98@gmail.com>
2022-04-29 20:11:58 -0300

    BaSP-W07-2022: Added login fetch and modal

--- Semana-07/js/login.js
+++ Semana-07/js/login.js
@@ -3,48 +3,79 @@ window.onload = () => {
   // inputs
   let inputEmail = document.querySelector('#email');
   let inputPassword = document.querySelector('#password');
-  let inputRememberMe = document.querySelector('#remember-me');
   // inputs icons
   let inputEmailIcon = document.querySelector('#email-icon');
   let inputPasswordIcon = document.querySelector('#password-icon');
   // button
   let buttonLogin = document.querySelector('#button-login');
+  // modal
+  let modal = document.getElementById("myModal");
+  let closeModal = document.getElementsByClassName("close-modal")[0];
+
+
 
 
   // Add events listeners
   inputEmail.addEventListener("blur", validateEmail);
   inputPassword.addEventListener("blur", validatePassword);
-  inputEmail.addEventListener("focus", removeEmailError);
-  inputPassword.addEventListener("focus", removePasswordError);
+
+  inputEmail.addEventListener("focus", removeError);
+  inputPassword.addEventListener("focus", removeError);
+
   buttonLogin.addEventListener("click", handleLogin);
 
 
   // Functions
+
+  // modal
+  closeModal.onclick = function () {
+    modal.style.display = "none";
+  }
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
   function handleLogin(e) {
     e.preventDefault();
     let email = inputEmail.value;
     let password = inputPassword.value;
-    alert('email: ' + email + '\n' + 'password: ' + password);
+    fetchLogin(email, password);
+    modal.style.display = "inline-block";
+    //alert('email: ' + email + '\n' + 'password: ' + password);
   }
 
-  function checkNumbersAndLetters(str) {
-    let hasNumber = false;
-    let hasLetter = false;
-    for (let i = 0; i < str.length; i++) {
-      if (parseFloat(str.charAt(i))) {
-        hasNumber = true;
-      }
-      if (!parseFloat(str.charAt(i)) && str.charAt(i) != " ") {
-        hasLetter = true;
-      }
-    }
-    if (hasNumber && hasLetter) {
-      return true;
-    } else {
-      return false;
-    }
+  async function fetchLogin(email, password) {
+    let url = 'https://basp-m2022-api-rest-server.herokuapp.com/login?';
+
+    let data = {
+      email: email,
+      password: password
+    };
+
+    let queryParams = new URLSearchParams(data);
+
+    console.log('first' + `${url}${queryParams}`);
+
+    fetch(`${url}${queryParams}`)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+      });
   }
 
+
+
+  function removeError(e) {
+    let input = e.currentTarget;
+    let icon = document.querySelector(`#${input.id}-icon`);
+    input.classList.remove('error');
+    icon.classList.remove('error');
+  }
+
+  // validation functions inputs
   function validateEmail(e) {
     let email = e.currentTarget.value;
     if (/[a-z0-9]+@[a-z]+\.[a-z]{2,3}/.test(email)) {
@@ -56,7 +87,7 @@ window.onload = () => {
 
   function validatePassword(e) {
     let password = e.currentTarget.value;
-    if (checkNumbersAndLetters(password) && password.length > 5) {
+    if (checkNumbersAndLetters(password) && password.length > 7) {
     }
     else {
       inputPassword.classList.add('error');
@@ -64,13 +95,4 @@ window.onload = () => {
     }
   }
 
-  function removeEmailError(e) {
-    inputEmail.classList.remove('error');
-    inputEmailIcon.classList.remove('error');
-  }
-
-  function removePasswordError(e) {
-    inputPassword.classList.remove('error');
-    inputPasswordIcon.classList.remove('error');
-  }
 }
